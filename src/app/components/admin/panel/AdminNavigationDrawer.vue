a41e889701c534d10eebe4d0960245782d8a03da
Elysium <61485096+Xrondev@users.noreply.github.com>
2023-02-04 11:52:18 +0800

    Update v-app
    update the v-app tag in Vuetify,
    the v-app-bar and drawer tag can only be contained inside the v-app tag

--- src/app/components/admin/panel/AdminNavigationDrawer.vue
+++ src/app/components/admin/panel/AdminNavigationDrawer.vue
@@ -1,32 +1,48 @@
 <template>
-  <v-app>
-  <v-app-bar>
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-toolbar-title>DriveIndex</v-toolbar-title>
-    <v-navigation-drawer
-      v-model="drawer"
-      temporary
-    >
-      <v-list-item
-        prepend-avatar="https://randomuser.me/api/portraits/men/78.jpg"
-        title="John Leider"
-      ></v-list-item>
+  <v-card>
+    <v-layout>
+      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
 
-      <v-divider></v-divider>
+      <v-app-bar
+        color="primary"
+        prominent
+      >
+        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-list density="compact" nav>
-        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
-        <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-  </v-app-bar>
-  </v-app>
+        <v-toolbar-title>My files</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn variant="text" icon="mdi-magnify"></v-btn>
+
+        <v-btn variant="text" icon="mdi-filter"></v-btn>
+
+        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+      </v-app-bar>
+
+      <v-navigation-drawer
+        v-model="drawer"
+        location="bottom"
+        temporary
+      >
+        <v-list
+          :items="items"
+        ></v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 500px;">
+        <v-card-text>
+          The navigation drawer will appear from the bottom on smaller size screens.
+        </v-card-text>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
 <script lang="ts" setup>
 import {ref} from "vue";
 
-const drawer = ref(false)
+let drawer
 </script>
 
 <style scoped>
--- src/app/views/Admin.vue
+++ src/app/views/Admin.vue
@@ -1,22 +1,24 @@
 <template>
-  <router-view/>
+  <v-app>
+    <router-view/>
+  </v-app>
 </template>
 
 <script lang="ts" setup>
 import {useI18n} from "vue-i18n";
 import {useRouter} from "vue-router";
-import {useStore} from "@/core/store";
 import {checkToken} from "@/core/requests/APIs";
+
 const router = useRouter()
 const {t} = useI18n()
 const token = localStorage.token
-if(token == undefined) {
+if (token == undefined) {
   router.push('/admin/login')
-}else{
+} else {
   checkToken(token).then((res) => {
-    if(res.code == 200) {
+    if (res.code == 200) {
       router.push('/admin/panel')
-    }else{
+    } else {
       router.push('/admin/login')
     }
   }).catch((err) => {
